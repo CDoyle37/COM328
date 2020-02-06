260013b7787fc58a074e40846f428a22a4a2d9ad
Caoimhin Doyle <doyle-c37@ulster.ac.uk>
2020-02-06 14:05:08 +0000

    Completed week 2 practical - searching
    Note: Did not implement the search array for entire other array function

--- Week 2/practical2/Search.h
+++ Week 2/practical2/Search.h
@@ -17,6 +17,13 @@
 // PostCondition: return position if key found, otherwise -1
 template <class T>
 int sequentialSearch( const Array<T> & items, const T & key ) {
+	// Loop through entire array of items
+	for (int i = 0; i < items.length(); i++) {
+		if (key == items[i]) {
+			std::cout << "Linear Search : [" << items << "] for " << key << " took " << i << " passes\n";
+			return i;
+		}
+	}
 	return -1;
 }
 
@@ -24,6 +31,34 @@ int sequentialSearch( const Array<T> & items, const T & key ) {
 // PostCondition: return position if key found, otherwise -1
 template <class T>
 int binarySearch(const Array<T> & items, const T & key) {
+	// Print the array being searched
+	std::cout << "Binary Search : [" << items << "] for " << key << "\n";
+
+	// Establish left and right bounds
+	int left = 0;
+	int right = items.length() - 1;
+
+	// Search through array, halving options each time
+	while (left <= right) {
+		// Calculate pivot
+		int pivot = (left + right) / 2;
+
+		// Each loop, print the left, right and pivot values
+		std::cout << "Left:" << left << "  Pivot:" << pivot << "  Right:" << right << "\n";
+
+		if (items[pivot] == key) {
+			// Item found
+			return pivot;
+		}
+		else if (items[pivot] >= key) {
+			// Pivot is too far right, shift all to left
+			right = pivot - 1;
+		}
+		else if (items[pivot] <= key) {
+			// Pivot is too far left, shift all to right
+			left = pivot + 1;
+		}
+	}
 	return -1;
 }
 
@@ -31,14 +66,34 @@ int binarySearch(const Array<T> & items, const T & key) {
 // PostCondition: return number of occurences of e in Array
 template <class T>
 int countOccurrence(const Array<T> & items, const T & key) {
-	return 0;
+	int total = 0;
+	int comps = 0;
+	for (int i = 0; i < items.length(); i++) {
+		if (items[i] == key) {
+			total++;
+		}
+		comps++;
+	}
+	std::cout << "Count for " << key << " in [" << items << "] took " << comps << " comparisons" << std::endl;
+	return total;
 }
 
 // PreCondition: Data array is full and sorted
 // PostCondition: return number of occurences of e in Array
 template <class T>
 int countOccurrenceOrdered(const Array<T> & items, const T & key) {
-	return 0;
+	int comps = 0;
+	int total = 0;
+	for (int i = 0; i < items.length(); i++) {
+		comps++;
+		// Find first occurence
+		if (items[i] == key) {
+			total++;
+			if (items[i + 1] != key) break;
+		}
+	}
+	std::cout << "Count Ordered for " << key << " in [" << items << "] took " << comps << " comparisons" << std::endl;
+	return total;
 }
 
 // PreCondition: Both Arrays are filled with values of type T. 
@@ -47,6 +102,14 @@ int countOccurrenceOrdered(const Array<T> & items, const T & key) {
 template <class T>
 bool containsAll(const Array<T> & items, const Array<T> & keys) {
 	return false;
+}
+
+template <class T>
+bool isOrdered(const Array<T>& items) {
+	for (int i = 1; i < items.length(); i++) {
+		if (items[i] < items[i - 1]) return false;
+	}
+	return true;
 }
 
 #endif	/* SEARCH_H_ */
--- Week 2/practical2/practical2_test.cpp
+++ Week 2/practical2/practical2_test.cpp
@@ -32,21 +32,56 @@ TEST_CASE("UnOrdered Search Axioms", "[Search]")
 		REQUIRE(sequentialSearch<int>(ua,5) == 0);
 
 		// assert that 20 is found at postion 9		
+		REQUIRE(sequentialSearch<int>(ua, 20) == 9);
 	}
 
 	SECTION("Sequential Search Failure")
 	{
-		//assert that 100 is not found 	
+		//assert that 100 is not found
+		REQUIRE(sequentialSearch<int>(ua, 100) == -1);
 	}
 
 	SECTION("Count occurence exists")
 	{
-		//assert that 3 is found twice	
+		//assert that 3 is found twice
+		REQUIRE(countOccurrence<int>(ua, 3) == 2);
 	}
 
 	SECTION("Count occurence non existent")
 	{
 		//assert that counting occurrence of 100 returns 0	
+		REQUIRE(countOccurrence<int>(ua, 100) == 0);
+	}
+}
+
+TEST_CASE("Ordered Search Axioms", "[Search]")
+{
+	Array<int> oa(10);
+	oa[0] = 3; oa[1] = 3; oa[2] = 5; oa[3] = 5; oa[4] = 7;
+	oa[5] = 9; oa[6] = 10; oa[7] = 11; oa[8] = 18; oa[9] = 20;
+
+	SECTION("Binary Search Success")
+	{
+		// Make sure array is sorted before searching
+		REQUIRE(isOrdered<int>(oa) == true);
+
+		//assert that 10 is found at position 6
+		REQUIRE(binarySearch<int>(oa, 10) == 6);
+
+		// assert that 20 is found at postion 9		
+		REQUIRE(binarySearch<int>(oa, 20) == 9);
+	}
+
+	SECTION("Binary Search Failure")
+	{
+		//assert that 100 is not found
+		REQUIRE(binarySearch<int>(oa, 100) == -1);
+	}
+
+	SECTION("Count occurence orderred")
+	{
+		//assert that counting occurrence of 5 returns 2
+		REQUIRE(countOccurrenceOrdered<int>(oa, 5) == 2);
 	}
 }
 
